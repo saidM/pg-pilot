76a79c35282132eb12dd15fa38478e78342a8c5b
Saïd <sayid.mimouni@gmail.com>
2016-10-01 14:34:23 +0200

    Implement query() route

--- server.js
+++ server.js
@@ -18,11 +18,27 @@ app.post('/login', (req, res, next) => {
     // Now get all the tables
     return database.getTables();
   }).then((data) => {
+    app.set('pg', database); // Save the Database instance (so we can re-use it in all the routes)
     res.json(data);
   }).catch((err) => {
+    app.set('pg', null); // Empty the PG client
     next({ status: 401, message: err });
   });
 });
+
+app.post('/query', isConnected, (req, res, next) => {
+  app.get('pg').query(req.body.sql).then((data) => {
+    res.send(data);
+  }).catch((err) => {
+    next({ status: 500, message: err });
+  });
+});
+
+
+function isConnected(req, res, next) {
+  if (!app.get('pg')) next({ status: 401, message: 'You must be connected to the database' });
+  next();
+}
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message);
---
+++ test/query_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai'),
+    expect = chai.expect,
+    request = require('supertest'),
+    app = require('../server');
+
+describe('POST /query', () => {
+  it('returns 401 if there is no active connection to the database', (done) => {
+    request(app)
+      .post('/login')
+      .send({ database: 'pokemon_test', user: 'postgres2' }) // Invalid credentials on purpose to empty the pg variable
+      .end((err, res) => {
+        request(app)
+          .post('/query')
+          .send({ sql: 'select 1+1' })
+          .expect(401, done);
+      });
+  });
+  
+  it('returns 500 if the query is invalid', (done) => {
+    request(app)
+      .post('/login')
+      .send({ database: 'pokemon_test', user: 'postgres' })
+      .end((err, res) => {
+        request(app)
+          .post('/query')
+          .send({ sql: 'abc' })
+          .expect(500, done);
+      });
+  });
+  
+  it('returns 200 if the query is valid', (done) => {
+    request(app)
+      .post('/login')
+      .send({ database: 'pokemon_test', user: 'postgres' })
+      .end((err, res) => {
+        request(app)
+          .post('/query')
+          .send({ sql: 'select 1+1' })
+          .expect(200, done);
+      });
+  });
+});
