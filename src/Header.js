815fe9824e66eed0264f1e78490d63d4348cfccd
Saïd <sayid.mimouni@gmail.com>
2016-12-02 05:35:37 +0100

    Add Link to Export route

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
           <li>PG-Pilot</li>
           <li><a href="">Perform SQL query</a></li>
           <li><a href="">Import File</a></li>
-          <li><a href="">Export Database</a></li>
+          <li><a href="http://localhost:8080/export">Export Database</a></li>
           <li className="right"><strong>profacture_dev</strong> <a href="">Switch database</a></li>
         </ul>
       </header>
