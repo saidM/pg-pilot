10f6915db8880c9c9a1fa7b567b96648d90e36a6
Saïd <sayid.mimouni@gmail.com>
2016-12-07 09:30:10 +0100

    Add dynamic port for Express backend

--- server.js
+++ server.js
@@ -103,6 +103,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message)
 });
 
-app.listen(8080)
+
+const port = process.argv[2] || 8080
+app.listen(port, () => { console.log(`PG-Pilot starting on http://localhost:${port}`) })
 
 module.exports = app
